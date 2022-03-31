b10f6586e246b6c873039a815902fc5ec0f01175
Ivan Lednev <bishop1860@gmail.com>
2022-03-31 19:12:59 +0200

    More test updates

--- src/parser/Parser.test.js
+++ src/parser/Parser.test.js
@@ -7,7 +7,13 @@ const DEFAULT_SETTINGS = {
     useTab: true,
     tabSize: 2,
 };
+
 const DEFAULT_INDENTATION = buildIndentation(DEFAULT_SETTINGS);
+
+// TODO: replace all copypaste
+function parse(lines, settings = DEFAULT_SETTINGS) {
+    return new SectionParser(new BlockParser(settings)).parse(lines);
+}
 
 test("Builds a flat structure with non-hierarchical text", () => {
     const lines = [
@@ -25,7 +31,7 @@ test("Builds a flat structure with non-hierarchical text", () => {
         "---",
     ];
 
-    const doc = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(lines);
+    const doc = parse(lines);
     expect(doc.blockContent.children.length).toBe(lines.length);
 });
 
@@ -33,7 +39,7 @@ describe("Headings", () => {
     test("Text after a heading gets nested", () => {
         const lines = ["# H1", "line"];
 
-        const root = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(lines);
+        const root = parse(lines);
 
         expect(root.children.length).toBe(1);
         const h1 = root.children[0];
@@ -43,7 +49,7 @@ describe("Headings", () => {
     test("A subheading creates another level of nesting", () => {
         const lines = ["# H1", "## H2", "line"];
 
-        const doc = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(lines);
+        const doc = parse(lines);
 
         const h1 = doc.children[0];
         expect(h1.children.length).toBe(1);
@@ -54,7 +60,7 @@ describe("Headings", () => {
     test("A same-level heading doesn't get nested", () => {
         const lines = ["# H1", "## H2", "## H2-2"];
 
-        const doc = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(lines);
+        const doc = parse(lines);
 
         const h1 = doc.children[0];
         expect(h1.children.length).toBe(2);
@@ -63,7 +69,7 @@ describe("Headings", () => {
     test("A higher-level heading pops nesting", () => {
         const lines = ["# H1", "## H2", "# H1", "line"];
 
-        const doc = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(lines);
+        const doc = parse(lines);
 
         expect(doc.children.length).toBe(2);
         const secondH1 = doc.children[1];
@@ -73,7 +79,7 @@ describe("Headings", () => {
     test("Doesn't break when the user jumps over a level", () => {
         const lines = ["# H1", "#### H4", "Text", "## H2"];
 
-        const doc = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(lines);
+        const doc = parse(lines);
         expect(doc.children.length).toBe(1);
         expect(doc.children[0].children.length).toBe(2);
     });
@@ -83,7 +89,7 @@ describe("List items", () => {
     test("Indented text after a list item gets nested", () => {
         const lines = ["- l", "\ttext"];
 
-        const doc = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(lines);
+        const doc = parse(lines);
         expect(doc.blockContent.children.length).toBe(1);
         const listItem = doc.blockContent.children[0];
         expect(listItem.children.length).toBe(1);
@@ -92,7 +98,7 @@ describe("List items", () => {
     test("An indented list item creates another level of nesting", () => {
         const lines = ["- l", "\t- l2", "\t\ttext"];
 
-        const doc = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(lines);
+        const doc = parse(lines);
         const listItem = doc.blockContent.children[0];
         const indentedListItem = listItem.children[0];
         expect(indentedListItem.children.length).toBe(1);
@@ -100,14 +106,14 @@ describe("List items", () => {
 
     test("A same level list item doesn't get nested", () => {
         const lines = ["- l", "\t- l2", "\t- l2-2"];
-        const doc = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(lines);
+        const doc = parse(lines);
         const listItem = doc.blockContent.children[0];
         expect(listItem.children.length).toBe(2);
     });
 
     test("A higher-level list item pops nesting", () => {
         const lines = ["- l", "\t- l2", "- l2-2"];
-        const doc = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(lines);
+        const doc = parse(lines);
         expect(doc.blockContent.children.length).toBe(2);
     });
 
@@ -120,33 +126,33 @@ describe("List items", () => {
             "    - 1b",
         ];
 
-        const doc = new SectionParser(
-            new BlockParser({
-                ...DEFAULT_SETTINGS,
-                useTab: false,
-                tabSize: 4,
-            })
-        ).parse(lines);
+        const doc = parse(lines, { useTab: false, tabSize: 4 });
         expect(doc.blockContent.children.length).toBe(1);
     });
 
     test("A top-level line breaks out of a list context", () => {
         const lines = ["- l", "\t- l2", "line"];
-        const doc = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(lines);
-        expect(doc.blockContent.children.length).toBe(2);
+        expect(parse(lines).blockContent.children).toMatchObject([
+            {
+                text: "- l",
+                children: [
+                    {
+                        text: "- l2",
+                    },
+                ],
+            },
+            {
+                text: "line",
+            },
+        ]);
     });
 
     test.each([
         [2, ["- l", "  - l2", "    text", "", "  Top-level text"]],
         [4, ["- l", "    - l2", "      text", "", "    Top-level text"]],
     ])("Indentation with spaces of different lengths: %d", (tabSize, lines) => {
-        const doc = new SectionParser(
-            new BlockParser({
-                ...DEFAULT_SETTINGS,
-                useTab: false,
-                tabSize: tabSize,
-            })
-        ).parse(lines);
+        const doc = parse(lines, { useTab: false, tabSize: tabSize });
+
         expect(doc.blockContent.children.length).toBe(3);
         const listItem = doc.blockContent.children[0];
         expect(listItem.children.length).toBe(1);
@@ -157,77 +163,70 @@ describe("List items", () => {
         ["Numbers", ["1. l", "\t11. l2", "\t\ttext"]],
         ["Mixed", ["1. l", "\t* l2", "\t\ttext"]],
     ])("Different types of list markers: %s", (message, lines) => {
-        const doc = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(lines);
-        expect(doc.blockContent.children.length).toBe(1);
-        const listItem = doc.blockContent.children[0];
-        expect(listItem.children.length).toBe(1);
-        const nestedListItem = listItem.children[0];
-        expect(nestedListItem.children.length).toBe(1);
+        expect(parse(lines).blockContent.children).toMatchObject([
+            {
+                text: expect.stringContaining("l"),
+                children: [
+                    {
+                        text: expect.stringContaining("l2"),
+                        children: [
+                            {
+                                text: "text",
+                            },
+                        ],
+                    },
+                ],
+            },
+        ]);
     });
 
     test("Handles misaligned lists", () => {
         const lines = ["- l", "  - text"];
 
-        const doc = new SectionParser(
-            new BlockParser({
-                ...DEFAULT_SETTINGS,
-                useTab: false,
-                tabSize: 4,
-            })
-        ).parse(lines);
+        const doc = parse(lines, { useTab: false, tabSize: 4 });
 
-        expect(doc).toMatchObject({
-            blockContent: {
+        expect(doc.blockContent.children).toMatchObject([
+            {
+                text: "- l",
                 children: [
                     {
-                        text: "- l",
-                        children: [
-                            {
-                                text: "- text",
-                            },
-                        ],
+                        text: "- text",
                     },
                 ],
             },
-        });
+        ]);
     });
 
     test("Handles indentation detection when tabs get mixed with spaces (the number of spaces matches the config)", () => {
         const lines = ["- 1", "\t- 1.1", "\t\t- 1.1.1", "    - 1.2"];
 
-        const doc = new SectionParser(
-            new BlockParser({ ...DEFAULT_SETTINGS, tabSize: 4 })
-        ).parse(lines);
+        const doc = parse(lines, { ...DEFAULT_SETTINGS, tabSize: 4 });
 
-        expect(doc).toMatchObject({
-            blockContent: {
+        expect(doc.blockContent.children).toMatchObject([
+            {
+                text: "- 1",
                 children: [
                     {
-                        text: "- 1",
+                        text: "- 1.1",
                         children: [
                             {
-                                text: "- 1.1",
-                                children: [
-                                    {
-                                        text: "- 1.1.1",
-                                    },
-                                ],
-                            },
-                            {
-                                text: "- 1.2",
+                                text: "- 1.1.1",
                             },
                         ],
                     },
+                    {
+                        text: "- 1.2",
+                    },
                 ],
             },
-        });
+        ]);
     });
 });
 
 describe("Mixing headings and lists", () => {
     test("One heading, one list", () => {
         const lines = ["# h", "- l", "line"];
-        const doc = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(lines);
+        const doc = parse(lines);
         expect(doc.children.length).toBe(1);
         const h1 = doc.children[0];
         expect(h1.blockContent.children.length).toBe(2);
@@ -235,7 +234,7 @@ describe("Mixing headings and lists", () => {
 
     test("Multiple heading levels", () => {
         const lines = ["# h", "- l", "text", "## h2", "# h1"];
-        const doc = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(lines);
+        const doc = parse(lines);
         expect(doc.children.length).toBe(2);
         const h1 = doc.children[0];
         expect(h1.children.length).toBe(1);
@@ -244,7 +243,7 @@ describe("Mixing headings and lists", () => {
 
     test("Multiple list levels", () => {
         const lines = ["# h", "- l", "\t- l2", "# h1"];
-        const doc = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(lines);
+        const doc = parse(lines);
         expect(doc.children.length).toBe(2);
         const h1 = doc.children[0];
         expect(h1.blockContent.children.length).toBe(1);
@@ -272,7 +271,7 @@ describe("Stringification", () => {
         ],
     ])("Round-tripping respects indentation settings: %s", (lines) => {
         const settings = { useTab: false, tabSize: 4 };
-        const parsed = new SectionParser(new BlockParser(settings)).parse(lines);
+        const parsed = parse(lines, settings);
         const stringified = parsed.stringify(buildIndentation(settings));
         expect(stringified).toEqual(lines);
     });
@@ -285,9 +284,7 @@ describe("Extraction", () => {
         const extracted = [["Extract me"]];
         const theRest = ["Text"];
 
-        const parsed = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(
-            lines
-        );
+        const parsed = parse(lines);
 
         const actual = parsed
             .extractBlocksRecursively({
@@ -304,9 +301,7 @@ describe("Insertion", () => {
     test("Append a block", () => {
         const lines = ["- list", "- text"];
 
-        const parsed = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(
-            lines
-        );
+        const parsed = parse(lines);
         parsed.blockContent.appendChild(new TextBlock("more text"));
         const stringified = parsed.stringify(DEFAULT_INDENTATION);
         expect(stringified).toEqual(["- list", "- text", "more text"]);
@@ -315,9 +310,7 @@ describe("Insertion", () => {
     test("Append a block to the start", () => {
         const lines = ["- list", "- text"];
 
-        const parsed = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(
-            lines
-        );
+        const parsed = parse(lines);
         parsed.blockContent.prependChild(new TextBlock("more text"));
         const stringified = parsed.stringify(DEFAULT_INDENTATION);
         expect(stringified).toEqual(["more text", "- list", "- text"]);
@@ -326,9 +319,7 @@ describe("Insertion", () => {
     test("Automatically adds indentation to a text block after a list item", () => {
         const lines = ["- list"];
 
-        const parsed = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(
-            lines
-        );
+        const parsed = parse(lines);
         parsed.blockContent.children[0].appendChild(new TextBlock("indented text"));
         expect(parsed.stringify(DEFAULT_INDENTATION)).toEqual([
             "- list",
@@ -341,9 +332,7 @@ describe("Insertion", () => {
 describe("Block search", () => {
     test("Find a block matching a matcher", () => {
         const lines = ["- list", "\t- text"];
-        const parsed = new SectionParser(new BlockParser(DEFAULT_SETTINGS)).parse(
-            lines
-        );
+        const parsed = parse(lines);
 
         const searchResult = findBlockRecursively(parsed.blockContent.children, (b) =>
             b.text.includes("text")
